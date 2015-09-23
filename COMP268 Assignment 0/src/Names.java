28f9986a10d230f7b9aec355946c6fa9bb24aec8
Neamnhi <classes@cawilson.ca>
2015-09-23 15:29:16 -0400

    Assignment 0 updates

--- COMP268 Assignment 0/src/Names.java
+++ COMP268 Assignment 0/src/Names.java
@@ -5,6 +5,30 @@
 
 // Assignment 0
 
-public class Names {
+/*
+	ASSIGNMENT
+	Create a class named “Names” that has the following field names. 
+		firstName, middleName, and lastName. 
+	Use appropriate datatypes to store values of these fields in Names objects. 
+	Create appropriate get and set methods to retrieve and assign values to these names.
+		For example, getMiddleName(vive) should return the middle name of the person Vive.
+		Similarly, vive.setLastName(lName) should set the last name of the person Vive
+		to the specified URL object. 
+	Test your program for correct, partially correct (e.g., name String without the middleName),
+		and incorrect inputs (e.g., name containing special characters). 
+	Document your program thoroughly. 
+	Create a profile of your program as you develop it (see “Program Profile” in Unit 0). 
+	Ensure that your program addresses all marking rubrics (see “Assessments” in Unit 0). 
+	Submit it to your tutor for marking. 
+ */
 
-}
+public class Names { // Opens class
+	
+	public static void main(String args[])
+	{ // Opens main
+		
+
+		
+	} // Closes main
+
+} // Closes class
